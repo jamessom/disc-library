9b912279a56276c66fcddd1c22abe153402bbb0f
jamessom <jamessomqueiroz@gmail.com>
2021-04-28 21:44:12 -0300

    feat(client): add axios and service api with axios interceptors

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,23 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import VueAxios from 'vue-axios';
+import { securedAxiosInstance, plainAxiosInstance } from './services/api';
 import './assets/css/main.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance,
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   components: { App },
   template: '<App/>',
 });
---
+++ client/src/services/api/index.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`;
+
+const baseConfig = {
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const securedAxiosInstance = axios.create(baseConfig);
+const plainAxiosInstance = axios.create(baseConfig);
+
+securedAxiosInstance.interceptors.request.use((config) => {
+  const method = config.method.toUpperCase();
+  if (method !== 'OPTIONIS' && method !== 'GET') {
+    config.headers = {
+      ...config.headers,
+      'X-CSRF-TOKEN': localStorage.csrf,
+    };
+  }
+
+  return config;
+});
+
+securedAxiosInstance.interceptors.request.use(null, (error) => {
+  const responseConfigStatus =
+    error.response &&
+    error.response.config &&
+    error.response.config.status === 401;
+
+  if (!responseConfigStatus) {
+    return Promise.reject(error);
+  }
+
+  return plainAxiosInstance
+    .post(
+      '/refresh',
+      {},
+      {
+        headers: {
+          'X-CSRF-TOKEN': localStorage.csrf,
+        },
+      },
+    )
+    .then((response) => {
+      localStorage.csrf = response.data.csrf;
+      localStorage.signedIn = true;
+
+      let retryConfig = error.response.config;
+      retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf;
+
+      return plainAxiosInstance.response(retryConfig);
+    })
+    .catch((error) => {
+      delete localStorage.csrf;
+      delete localStorage.signedIn;
+
+      location.replace('/');
+
+      return Promise.reject(error);
+    });
+});
+
+export { securedAxiosInstance, plainAxiosInstance };
