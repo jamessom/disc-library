7214699a53c7c756b029b92c7bff9b44a990890f
jamessom <jamessomqueiroz@gmail.com>
2021-05-19 12:23:59 -0300

    fix(backend): fix controller assign in route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
 
   post   'refresh', controller: :refresh, action: :create
-  post   'signin',  controller: :refresh, action: :create
-  post   'signup',  controller: :refresh, action: :create
-  delete 'signin',  controller: :refresh, action: :destroy
+  post   'signin',  controller: :signin, action: :create
+  post   'signup',  controller: :signup, action: :create
+  delete 'signin',  controller: :signin, action: :destroy
 end
