31de679f7e605fbb8fc1d8f2be4d136ae280e7e9
jamessom <jamessomqueiroz@gmail.com>
2021-05-19 12:24:43 -0300

    test(backend): test env

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -1,4 +1,8 @@
+require_relative 'support/response_helper'
+
 RSpec.configure do |config|
+  config.include ResponseHelper
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
